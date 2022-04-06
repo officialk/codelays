db231eb854eb2b5261cac60a1bf0bbe53a8b6d6a
Akash <akashpal9007@gmail.com>
2022-04-06 18:31:59 +0530

    CU-257ktd2 - create the component-loader

---
+++ src/Components/Loader/Loader.stories.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Loader } from './Loader';
+
+export default {
+    title: 'Example/Loader',
+    component: Loader,
+};
+
+const Template = (args) => <Loader {...args} />;
+
+export const Primary = Template.bind({});
