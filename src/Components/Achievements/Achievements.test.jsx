7d2e8170b2e6fb450d5a22e920f776d80ac7fccb
Akash <akashpal9007@gmail.com>
2022-04-07 19:43:29 +0530

    testing-achivements-component

---
+++ src/Components/Achievements/Achievements.test.jsx
@@ -0,0 +1,16 @@
+// import { render } from '@testing-library/react';
+// import '@testing-library/jest-dom'
+// import { Achievements } from './index.jsx';
+
+// describe("Achievement Component", () => {
+
+//     it('Checks if the Achievements renders when value is true', () => {
+//       render(<Achievements achievement={true} />);
+//       expect(<Achievements />).toBeInTheDocument();
+//     });
+
+//     it('Checks if the Achievements !renders when value is false', () => {
+//       render(<Achievements achievement={false} />);
+//       expect(<Achievements />).Not.toBeInTheDocument();
+//     });
+// })
--- src/Components/Achievements/index.jsx
+++ src/Components/Achievements/index.jsx
@@ -5,12 +5,12 @@ import './Achievements.css';
 export const Achievements = ({achievement, achievementTitle, image, description}) => {
   return (
     <>
-      {achievement && (<div className='achievement'>
+      {achievement && (<div data-testid="loadAchievements" className='achievement'>
           <img id='achievement-img' src={image} alt="" />
           <div className='achievement-text'>
             <span className="ach-span1">{achievementTitle}</span>
             <span className='ach-span2'>{description}</span>
-          </div>
+          </div> 
       </div>)}
     </>
   );
