28137a87acf1a9ac135d3c08f7530e7794acd165
Akash <akashpal9007@gmail.com>
2022-04-08 17:32:22 +0530

    tested achievement componenet

--- src/Components/Achievements/Achievements.test.jsx
+++ src/Components/Achievements/Achievements.test.jsx
@@ -1,16 +1,12 @@
-// import { render } from '@testing-library/react';
-// import '@testing-library/jest-dom'
-// import { Achievements } from './index.jsx';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import { Achievements } from './index.jsx';
 
-// describe("Achievement Component", () => {
+describe("Achievement Component", () => {
 
-//     it('Checks if the Achievements renders when value is true', () => {
-//       render(<Achievements achievement={true} />);
-//       expect(<Achievements />).toBeInTheDocument();
-//     });
-
-//     it('Checks if the Achievements !renders when value is false', () => {
-//       render(<Achievements achievement={false} />);
-//       expect(<Achievements />).Not.toBeInTheDocument();
-//     });
-// })
+    it('Checks if the Achievements renders when value is true', () => {
+        const { getByTestId } = render(<Achievements achievement= {true} />);
+        const achievementComponent = getByTestId('testAchievements');
+        expect(achievementComponent).toBeInTheDocument();
+    });
+})
--- src/Components/Achievements/index.jsx
+++ src/Components/Achievements/index.jsx
@@ -1,11 +1,10 @@
 import { React, useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
 import './Achievements.css';
 
 export const Achievements = ({achievement, achievementTitle, image, description}) => {
   return (
     <>
-      {achievement && (<div data-testid="loadAchievements" className='achievement'>
+      {achievement && (<div data-testid="testAchievements" className='achievement'>
           <img id='achievement-img' src={image} alt="" />
           <div className='achievement-text'>
             <span className="ach-span1">{achievementTitle}</span>
@@ -14,18 +13,4 @@ export const Achievements = ({achievement, achievementTitle, image, description}
       </div>)}
     </>
   );
-};
-
-Achievements.propTypes = {
-  achievement: PropTypes.bool,
-  achievementTitle: PropTypes.string,
-  image: PropTypes.string,
-  description: PropTypes.string,
-};
-
-Achievements.defaultProps = {
-  achievement: true,
-  achievementTitle: "Achievement Unlocked",
-  image: null,
-  description: null,
 };
