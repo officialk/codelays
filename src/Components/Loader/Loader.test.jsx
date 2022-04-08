4916e86a82c73c886e1e521e8df65f78ab49f78e
Akash <akashpal9007@gmail.com>
2022-04-08 19:28:11 +0530

    audited loader

--- src/Components/Loader/Loader.test.jsx
+++ src/Components/Loader/Loader.test.jsx
@@ -1,21 +1,20 @@
 import { render } from '@testing-library/react';
-import '@testing-library/jest-dom'
-import { act } from "react-dom/test-utils";
+import '@testing-library/jest-dom';
 import { Loader } from './index.jsx';
 
 const props = {color: '#d30404', percent: '30'};
 
-const { getByTestId } = render(<Loader {...props} />);
-const isLoaderValid = getByTestId('loadProgressBar');
-const loaderComponent = getByTestId('loader');
 it('Checks if the Loader renders', () => {
+  const { getByTestId } = render(<Loader {...props} />);
+  const isLoaderValid = getByTestId('loadProgressBar');
+  const loaderComponent = getByTestId('loader');
   expect(isLoaderValid).toBeInTheDocument();
   expect(loaderComponent).toHaveStyle(`backgroundColor: ${props.color}`);
   expect(loaderComponent).toHaveStyle(`width: ${props.percent}%`);
 });
 it('Checks the Loader in realtime with the prop passed', () => {
-  act(() => {
-    render(<Loader percent='90' />);
-  });
+    const { getByTestId } = render(<Loader percent='90' color="#9f6262" />);
+    const loaderComponent = getByTestId('loader');
   expect(loaderComponent).toHaveStyle('width: 90%');
+  expect(loaderComponent).toHaveStyle('backgroundColor: #9f6262');
 });
