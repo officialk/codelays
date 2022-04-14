644e2cf6a893a80c0c8c805968eed58c913f0eb3
Akash <akashpal9007@gmail.com>
2022-04-14 14:55:47 +0530

    upadated loader.test.jsx

--- src/Components/Loader/Loader.test.jsx
+++ src/Components/Loader/Loader.test.jsx
@@ -1,20 +1,29 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Loader } from './index.jsx';
+import userEvent from '@testing-library/user-event';
 
-const props = {color: '#d30404', percent: '30'};
+const props = { percent: '30'};
 
-it('Checks if the Loader renders', () => {
-  const { getByTestId } = render(<Loader {...props} />);
-  const isLoaderValid = getByTestId('loadProgressBar');
-  const loaderComponent = getByTestId('loader');
-  expect(isLoaderValid).toBeInTheDocument();
-  expect(loaderComponent).toHaveStyle(`backgroundColor: ${props.color}`);
-  expect(loaderComponent).toHaveStyle(`width: ${props.percent}%`);
-});
+// it('Checks if the Loader renders', () => {
+//   const { getByTestId } = render(<Loader {...props} />);
+//   const isLoaderValid = getByTestId('loadProgressBar');
+//   const loaderComponent = getByTestId('loader');
+//   expect(isLoaderValid).toBeInTheDocument();
+//   expect(loaderComponent).toHaveStyle(`width: ${props.percent}%`);
+// });
+// it('Checks the Loader in realtime with the prop passed', () => {
+//     const { getByTestId } = render(<Loader percent='90'/>);
+//     const loaderComponent = getByTestId('loader');
+//   expect(loaderComponent).toHaveStyle('width: 90%');
+//   expect(loaderComponent).toHaveStyle('backgroundColor: #d30404');
+// });
 it('Checks the Loader in realtime with the prop passed', () => {
-    const { getByTestId } = render(<Loader percent='90' color="#9f6262" />);
-    const loaderComponent = getByTestId('loader');
+  const { getByTestId } = render(<Loader percent='90' />);
+  const loaderComponent = getByTestId('loader');
   expect(loaderComponent).toHaveStyle('width: 90%');
-  expect(loaderComponent).toHaveStyle('backgroundColor: #9f6262');
+  userEvent.click[screen.getByRole('button', {name: /change/i})];
+  screen.debug();
+  expect(screen.getByTestId('loader').style.backgroundColor).toBe('#232dbb');
+  // expect(screen.getByTestId('loader').style.backgroundColor).toHaveStyle('backgroundColor: #232dbb');
 });
--- src/Components/Loader/index.jsx
+++ src/Components/Loader/index.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import {React, useState} from 'react';
 import './Loader.css';
 
-export const Loader = ({color, percent}) => {
+export const Loader = ({percent}) => {
+  const [color, setColor] = useState("#d30404");
+  const toggleTheme = () => {
+    setColor("#232dbb");
+  };
   return (
     <>
       <div data-testid="loadProgressBar" className="progressBar">
         <span data-testid="loader" style={{backgroundColor: `${color}`, width: `${percent}%`}} className="loader"></span>
       </div>
+      <button name='change' onClick={toggleTheme}>changeColor</button>
     </>
   );
 };
