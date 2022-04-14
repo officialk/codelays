238f57886ef9bf514426efcb4b01b1b36d24f40d
Akash <akashpal9007@gmail.com>
2022-04-14 15:22:32 +0530

    updated index.jsx

--- src/Components/Alerts/index.jsx
+++ src/Components/Alerts/index.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import './Alerts.css';
 
-let userImage = "https://images.unsplash.com/photo-1551143899-e3d08acc5b00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80";
-let userName = "Howling_hood";
-let description = "Attacked your village and did 400 Damage!";
-
-export const Alerts = ({ open, userImage, userName, description}) => {
+export const Alerts = ({ open}) => {
+  let userImage = "https://images.unsplash.com/photo-1551143899-e3d08acc5b00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80";
+  let userName = "Howling_hood";
+  let description = "Attacked your village and did 400 Damage!";
   return (
     <>
       {open && <div className="Alerts" data-testid="loadAlerts" >
-          <img id='userImg' src={userImage} alt="" />
+          <img id='alerts-userImg' src={userImage} alt="" />
           <div className='Alerts-text'>
               <span>{userName}</span>
               <span>{description}</span>
