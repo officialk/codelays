0943771e35f5e1e1e0f9c3a0ba52c1e3c5dc4a58
Akash <akashpal9007@gmail.com>
2022-04-14 18:55:54 +0530

    updated Loader.test.jsx

--- src/Components/Loader/Loader.test.jsx
+++ src/Components/Loader/Loader.test.jsx
@@ -1,29 +1,21 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Loader } from './index.jsx';
-import userEvent from '@testing-library/user-event';
 
 const props = { percent: '30'};
 
-// it('Checks if the Loader renders', () => {
-//   const { getByTestId } = render(<Loader {...props} />);
-//   const isLoaderValid = getByTestId('loadProgressBar');
-//   const loaderComponent = getByTestId('loader');
-//   expect(isLoaderValid).toBeInTheDocument();
-//   expect(loaderComponent).toHaveStyle(`width: ${props.percent}%`);
-// });
-// it('Checks the Loader in realtime with the prop passed', () => {
-//     const { getByTestId } = render(<Loader percent='90'/>);
-//     const loaderComponent = getByTestId('loader');
-//   expect(loaderComponent).toHaveStyle('width: 90%');
-//   expect(loaderComponent).toHaveStyle('backgroundColor: #d30404');
-// });
+it('Checks if the Loader renders', () => {
+  const { getByTestId } = render(<Loader {...props} />);
+  const isLoaderValid = getByTestId('loadProgressBar');
+  const loaderComponent = getByTestId('loader');
+  expect(isLoaderValid).toBeInTheDocument();
+  expect(loaderComponent).toHaveStyle(`width: ${props.percent}%`);
+});
 it('Checks the Loader in realtime with the prop passed', () => {
   const { getByTestId } = render(<Loader percent='90' />);
-  const loaderComponent = getByTestId('loader');
-  expect(loaderComponent).toHaveStyle('width: 90%');
-  userEvent.click[screen.getByRole('button', {name: /change/i})];
+  const loader = getByTestId('loader');
+  const btn = getByTestId('btn');
+  fireEvent.click(btn);
   screen.debug();
-  expect(screen.getByTestId('loader').style.backgroundColor).toBe('#232dbb');
-  // expect(screen.getByTestId('loader').style.backgroundColor).toHaveStyle('backgroundColor: #232dbb');
+  expect(loader).toHaveStyle('backgroundColor: #232dbb');
 });
--- src/Components/Loader/index.jsx
+++ src/Components/Loader/index.jsx
@@ -3,15 +3,12 @@ import './Loader.css';
 
 export const Loader = ({percent}) => {
   const [color, setColor] = useState("#d30404");
-  const toggleTheme = () => {
-    setColor("#232dbb");
-  };
   return (
     <>
       <div data-testid="loadProgressBar" className="progressBar">
         <span data-testid="loader" style={{backgroundColor: `${color}`, width: `${percent}%`}} className="loader"></span>
       </div>
-      <button name='change' onClick={toggleTheme}>changeColor</button>
+      <button data-testid="btn" onClick={()=>{setColor("#232dbb")}}>changeColor</button>
     </>
   );
 };
