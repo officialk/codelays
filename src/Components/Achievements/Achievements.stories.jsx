acbb2e76112c129265366f9b6b81e33d81a3fd86
Akash <akashpal9007@gmail.com>
2022-04-14 17:43:09 +0530

    updated index.jsx

--- src/Components/Achievements/Achievements.stories.jsx
+++ src/Components/Achievements/Achievements.stories.jsx
@@ -2,18 +2,13 @@ import React from 'react';
 import { Achievements } from './index.jsx';
 
 export default {
-    title: 'Example/Achievements',
+    title: 'Achievements',
     component: Achievements,
     parameters: {
         layout: 'centered',
       },
 };
 
-const Template = (args) => <Achievements {...args}/>;
+const Template = (args) => <Achievements />;
 
 export const Default = Template.bind({});
-Default.args = {
-    title: "Achievement Unlocked",
-    image: "https://images.unsplash.com/photo-1551143899-e3d08acc5b00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-    description: "Get it on steam",
-}
--- src/Components/Achievements/index.jsx
+++ src/Components/Achievements/index.jsx
@@ -1,7 +1,11 @@
 import { React, useState, useEffect } from 'react';
 import './Achievements.css';
 
-export const Achievements = ({ title, image, description}) => {
+export const Achievements = () => {
+  let title = "Achievement Unlocked";
+  let image = "https://images.unsplash.com/photo-1551143899-e3d08acc5b00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80";
+  let description = "Get it on steam";
+  
   const [achievement,setAchievement] = useState(true);
   useEffect(() => {
     setTimeout(() => {
@@ -12,7 +16,7 @@ export const Achievements = ({ title, image, description}) => {
   }, []);
   return (
     <>
-      {achievement && (<div data-testid="testAchievements" className='achievement'>
+      {achievement && (<div data-testid="testAchievements" className="achievement" >
           <img id='achievement-img' src={image} alt="" />
           <div className='achievement-text'>
             <span className="ach-span1">{title}</span>
