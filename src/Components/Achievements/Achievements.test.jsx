e564d72bc95ef12ea6d7f1fa625eed38c70be61c
Akash <akashpal9007@gmail.com>
2022-04-18 19:40:10 +0530

    updated achivement.test.jsx

--- src/Components/Achievements/Achievements.test.jsx
+++ src/Components/Achievements/Achievements.test.jsx
@@ -1,17 +1,20 @@
-import { queryByTestId, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom'
 import { Achievements } from './index.jsx';
 
 describe("Achievement Component", () => {
 
     it('Checks if the Achievements renders when value is true', () => {
-        const { getByTestId } = render(<Achievements achievement= {true} />);
+        const { getByTestId } = render(<Achievements />);
         const achievementComponent = getByTestId('testAchievements');
         expect(achievementComponent).toBeInTheDocument();
     });
-    it('Checks if the Achievements does Not renders when value is false', () => {
-        const { queryByTestId } = render(<Achievements achievement= {false} />);
-        const achievementComponent =  queryByTestId('testAchievements');
-        expect(achievementComponent).not.toBeInTheDocument();
+    it('Checks if the Achievements renders when value is true', () => {
+        const { getByTestId } = render(<Achievements achievement= {true} />);
+        const achievementComponent = getByTestId('testAchievements');
+        expect(achievementComponent).toBeInTheDocument();
+        setTimeout(function (){
+            expect(achievementComponent).not.toBeInTheDocument();
+        }, 10000)
     });
 })
