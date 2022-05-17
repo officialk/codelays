22e10bcdfbfe079804829d18f401ad83ae7c0535
akashpal <akashpal9007@gmail.com>
2022-05-17 17:52:52 +0530

    updated config & utils

--- src/utils/general_functions.jsx
+++ src/utils/general_functions.jsx
@@ -3,9 +3,8 @@
  * @param {number} time - time in seconds
  * @return {null}
  */
-const wait = async (time) =>
+export const wait = async (time) =>
     new Promise((resolve) => {
       setTimeout(resolve, time*1000);
     });
 
-export {wait};
