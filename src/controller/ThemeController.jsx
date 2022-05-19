d8fb38f12eee2e9226edcd32f6c1616d77f55541
akashpal <akashpal9007@gmail.com>
2022-05-19 17:36:27 +0530

    added controller

---
+++ src/controller/ThemeController.jsx
@@ -0,0 +1,13 @@
+// setSecondary takes the rgb value of background as parameter and returns the text color
+// @params {array}
+// @return {string}
+/**
+ * setSecondary sets the secondary color on the basis of r,g,b values of primary color passed
+ * @param {number} r - value of r of primary color
+ * @param {number} g - value of g of primary color
+ * @param {number} b - value of b of primary color
+ * @return {String} - returns white of black color's hex value 
+ */
+const setSecondary = (r, g, b) => (r * 299 + g * 587 + b * 114) / 1000 > 125 ? '#000000' : '#ffffff';
+
+export {setSecondary};
